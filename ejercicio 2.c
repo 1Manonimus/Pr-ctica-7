3acb2f35101812c979881cd352ebb83ba62caa6a
Elver Galarga <mangoancestral@gmail.com>
2025-03-27 14:09:49 -0600

    Create ejercicio 2.c

---
+++ ejercicio 2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+int a;
+printf("introduce un número\n");
+scanf("%i",&a);
+{
+if(a%2==0)
+{
+    printf("El número es par");
+}
+else
+{
+    printf("el número es impar");
+}
+}
+
+    return 0;
+}
