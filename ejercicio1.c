4e759181e8aaa108a84731b68def780caab813d2
Elver Galarga <mangoancestral@gmail.com>
2025-03-27 13:48:14 -0600

    Create ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+printf("introduce el primer números\n");
+scanf("%d",&a);
+printf("introduce el segundo números\n");
+scanf("%d",&b);
+{
+    if(a<b)
+    {
+        printf("el segundo número es mayor");
+    }
+    else
+    {
+        printf("el primer valor es mayor");
+    }
+}
+
+    return 0;
+}
